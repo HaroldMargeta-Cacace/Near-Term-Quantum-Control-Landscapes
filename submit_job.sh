d195f3bff08afc8f25067b289801e41f984165b1
Harrison-Copp <107236266+Harrison-Copp@users.noreply.github.com>
2025-07-11 21:05:13 -0400

    bash file for batch job on hoffman2

---
+++ submit_job.sh
@@ -0,0 +1,39 @@
+#### submit_job.sh START ####
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o joblog.$JOB_ID
+#$ -j y
+## Edit the line below as needed:
+#$ -l h_rt=2:00:00,h_data=1G
+## Modify the parallel environment
+## and the number of cores as needed:
+#$ -pe shared 8
+# Email address to notify
+#$ -M $USER@mail
+# Notify when
+#$ -m bea
+
+# echo job info on joblog:
+echo "Job $JOB_ID started on:   " `hostname -s`
+echo "Job $JOB_ID started on:   " `date `
+echo " "
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+## Edit the line below as needed:
+module load conda
+module load python/3.9.6
+conda activate mycondaenv1
+
+## substitute the command to run your code
+## in the two lines below:
+#echo '/usr/bin/time -v hostname'
+#/usr/bin/time -v hostname
+python QAOA.py
+
+# echo job info on joblog:
+echo "Job $JOB_ID ended on:   " `hostname -s`
+echo "Job $JOB_ID ended on:   " `date `
+echo " "
+#### submit_job.sh STOP ####
