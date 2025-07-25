33616e656913ab9c518390e1bf0103c3ad81bf06
Harrison-Copp <107236266+Harrison-Copp@users.noreply.github.com>
2025-07-25 16:57:32 -0700

    Update grad_sampling.py

--- BFGS Simulations/grad_sampling.py
+++ BFGS Simulations/grad_sampling.py
@@ -9,15 +9,16 @@ def grad(f, x, cur_calls=0, track_calls=True):
     cur_calls += 2 * len(x)
     return nd.Gradient(f)(x), cur_calls
 
-file = "QAOA_bfgs_Order_1-7_p1-7"
+file = "toy_data"
 with open(file + ".pkl", 'rb') as f:
     df = pickle.load(f)
 
-n_samples = 250
+n_samples = 100
 
 # Preallocate output arrays
 loss_var = np.zeros(len(df))
 grad_var = np.zeros(len(df))
+grad_var_components = []
 
 loop = tqdm(range(len(df)), desc="Starting...", position=0)
 
@@ -40,11 +41,16 @@ for i in loop:
         grad_eval, _ = grad(f, param, track_calls=False)
         grad_evaluations[j] = grad_eval
 
-    loss_var[i] = np.var(loss_evaluations, ddof=1)
-    grad_var[i] = np.mean(np.var(grad_evaluations, axis=0, ddof=1))
+    loss_var[i] = np.var(loss_evaluations, ddof=1) # loss variance
+    components = np.var(grad_evaluations, axis=0, ddof=1) # component variance
+    grad_var_components.append(components)
+    grad_var[i] = np.mean(components) # mean component variance
 
 df['sample_loss_var'] = loss_var
 df['sample_grad_var'] = grad_var
+df['sample_gar_var_components'] = grad_var_components
 
 df.to_csv(file + '_wvars.csv')
 df.to_pickle(file + '_wvars.pkl')
+
+
